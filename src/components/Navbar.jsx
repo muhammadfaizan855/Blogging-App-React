222a9440ea546ee2def57a683d72a380038c4cb4
Muhammadfaizan39 <fiazansuri855@gmail.com>
2024-09-30 03:49:07 +0500

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { auth } from '../config/firebase/firebasemethod';
 
 const Navbar = () => {
+  
+
+  const [userLogin , setUserLogin] = useState(false);
+ 
+  const navigate = useNavigate()
+  
+  useEffect(()=>{
+
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log(uid);
+        setUserLogin(true)
+      } else {
+       console.log("user login not");
+       
+      }
+    });
+  } , [])
+
+
+  const LoginBtn = ()=>{
+    navigate("/login")
+  }
+
+  const userLogout = ()=>{
+    signOut(auth)
+    .then(() => {
+      navigate("/login")  
+    }).catch((error) => {
+      alert("err" , error);
+    });    
+  }
+
   return (
     <>
   
@@ -9,12 +45,16 @@ const Navbar = () => {
   <div className="flex-1">
     <a className="btn btn-ghost text-xl text-white">Blogging App</a>
   </div>
-      <div className='d-flex justify-center gap-5 mx-5 text-white'>
+      <div>
+     {userLogin ? (
+      <div className='flex justify-center gap-5 mx-5 text-white'>
       <p><Link to=''>Home</Link></p>
       <p><Link to='dashboard'>Dashboard</Link></p>
       <p><Link to='profile'>Profile</Link></p>
-      <p><Link to='login'>Login</Link></p>
-      <p><Link to='register'>Register</Link></p>
+      <button onClick={userLogout}>Logout</button>
+      </div>
+     ) : 
+     <button className='btn mx-[30px]' onClick={LoginBtn}>Login</button>}
       </div>
      
 </div>
